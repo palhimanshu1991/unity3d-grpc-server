27d2ceb906e6a99599f95809107f528d54c3cbc1
Palhimanshu1991 <pal.himanshu1991@gmail.com>
2024-05-01 19:22:31 +0530

    first commit

---
+++ Assets/GrpcServerTest.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Grpc.Core;
+using System.Threading.Tasks;
+
+public class GrpcServerTest : MonoBehaviour
+{
+    Server server;
+
+    const string Host = "localhost";
+    const int Port = 88888;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        server = new Server
+        {
+            Services = { Helloworld.Greeter.BindService(new GrpcServiceImpl()) },
+            Ports = { new ServerPort(Host, Port, ServerCredentials.Insecure) }
+        };
+
+        Debug.Log("GrpcServerTest Start");
+
+        server.Start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnDestroy()
+    {
+        server.ShutdownAsync().Wait();
+    }
+
+    private void OnApplicationQuit()
+    {
+        server.ShutdownAsync().Wait();
+    }
+}
+
+public class GrpcServiceImpl : Helloworld.Greeter.GreeterBase
+{
+    public override Task<Helloworld.HelloReply> SayHello(Helloworld.HelloRequest request, ServerCallContext context)
+    {
+        return Task.FromResult(new Helloworld.HelloReply
+        {
+            Message = "Hello " + request.Name
+        });
+    }
+}
